1959442b070268b5aea01c23afb5f59ad9f73b94
kattd <kattd@gmail.com>
2024-01-20 19:59:18 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text("wellcome to 30 days of flutter"),
+        ),
+      ),
+    );
+  }
+}
